5b31b626eb107a81c041394a20a5ab7676de5621
Nathan Goenner <ngoenner@iastate.edu>
2023-12-06 20:00:35 -0600

    FPGA and heap memory

--- main.c
+++ main.c
@@ -28,23 +28,32 @@
 #include "dma.h"
 #include "sleep.h"
 #include <time.h>
-// #include "xtime_l.h"
+#include <stdlib.h>
+#include <stdarg.h>
 
-
-#define FILTER_DATA_SIZE (512*16)
+#define USE_FPGA 0
+#define FILTER_DATA_SIZE (1024)
 #define FILTER_LENGTH (21)
 #define ARRAY_LEN(x) sizeof(x)/sizeof(x[0])
 #define PI (3.1415926535897)
 #define SAMPLE_RATE (10000000) // 10 MHz
+
+void debug_out(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    char str[256];
+    vsnprintf(str, sizeof(str), format, args);
+    xil_printf("%s", str);
+
+}
 
 void printSamplesCSV(double* input, double* output, uint32_t length)
 {
     xil_printf("\r\nSample,In,Out");
     for (uint32_t i = 0; i < length; i++)
     {
-        char str[128];
-        snprintf(str, ARRAY_LEN(str), "\r\n%lu,%.4f,%.4f", i, input[i], output[i]);
-        xil_printf("%s", str);
+        debug_out("\r\n%lu,%.4f,%.4f", i, input[i], output[i]);
     }
 }
 
@@ -53,6 +62,7 @@ void genInputSamples(double* input, uint32_t length, double frequency, double am
     for (uint32_t i = 0; i < length; i++)
     {
         input[i] += sin(fs * i * 2 * PI * frequency) * amplitude;
+        // debug_out("Gen %d: variable=%0.2f", i, input[i]);
     }
 }
 
@@ -104,9 +114,7 @@ void test_time(u32 delay)
     usleep(delay);
     XTime_GetTime(&end);
 
-    char str[128];
-    sprintf(str, "\r\nDelay: %u, Time: %0.4f, Start: %llu, End: %llu", delay, time_diff_us(start, end), start, end);
-    xil_printf("%s", str);
+    debug_out("\r\nDelay: %u, Time: %0.4f, Start: %llu, End: %llu", delay, time_diff_us(start, end), start, end);
 }
 
 /* low pass cutoff w=0.2 (1 MHz) */
@@ -135,19 +143,38 @@ static double h[FILTER_LENGTH] = {
 };
 int main()
 {
-    double x[FILTER_DATA_SIZE] = {0.0};
-    double y[FILTER_DATA_SIZE] = {1.0};
-    fixed x_fixed[FILTER_DATA_SIZE] = {0};
-    fixed y_fixed[FILTER_DATA_SIZE] = {0};
+    init_platform();
+    XilTickTimer_Init(&TimerInst);
+    // double x[FILTER_DATA_SIZE] = {0.0};
+    // double y[FILTER_DATA_SIZE] = {1.0};
+    // fixed x_fixed[FILTER_DATA_SIZE] = {0};
+    // fixed y_fixed[FILTER_DATA_SIZE] = {0};
     fixed h_fixed[FILTER_LENGTH] = {0};
     fixed w_fixed[FILTER_LENGTH] = {0};
-    init_platform();
-    xtimerinit();
+
+    double* x = malloc(sizeof(double) * FILTER_DATA_SIZE);
+    double* y = malloc(sizeof(double) * FILTER_DATA_SIZE);
+    fixed* x_fixed = malloc(sizeof(fixed) * FILTER_DATA_SIZE);
+    fixed* y_fixed = malloc(sizeof(fixed) * FILTER_DATA_SIZE);
+    if (!x || !y || !x_fixed || !y_fixed)
+    {
+        debug_out("\r\nFailed to allocate memory!!! Pointers: x:%p y%p xf%p yf%p", x, y, x_fixed, y_fixed);
+        return 1;
+    }
+    clearArray_Double(x, FILTER_DATA_SIZE);
+    clearArray_Double(y, FILTER_DATA_SIZE);
+    clearArray_Fixed(x_fixed, FILTER_DATA_SIZE);
+    clearArray_Fixed(y_fixed, FILTER_DATA_SIZE);
+
+    // x[0] = 23.0;
+    // x[1] = 25.0;
 
     /* generate input signal: 500 KHz signal of interest, 3.3 & 5.1 MHz noise of 5 & 10 times size */
-    genInputSamples(&x[0], ARRAY_LEN(x),  500000., 1.,  1.0/SAMPLE_RATE);
-    genInputSamples(&x[0], ARRAY_LEN(x), 3300000., 10., 1.0/SAMPLE_RATE);
-    genInputSamples(&x[0], ARRAY_LEN(x), 5100000., 5.,  1.0/SAMPLE_RATE);
+    genInputSamples(x, FILTER_DATA_SIZE,  500000., 1.,  1.0/SAMPLE_RATE);
+    genInputSamples(x, FILTER_DATA_SIZE, 3300000., 10., 1.0/SAMPLE_RATE);
+    genInputSamples(x, FILTER_DATA_SIZE, 5100000., 5.,  1.0/SAMPLE_RATE);
+
+
 
     // test_time(1);
     // test_time(100);
@@ -158,33 +185,51 @@ int main()
     XTime startTime;
     XTime endTime;
     
-    floatToFixed(x, x_fixed, ARRAY_LEN(x));
-    floatToFixed(y, y_fixed, ARRAY_LEN(y));
+    floatToFixed(x, x_fixed, FILTER_DATA_SIZE);
+    floatToFixed(y, y_fixed, FILTER_DATA_SIZE);
     floatToFixed(h, h_fixed, ARRAY_LEN(h));
 
-    xil_printf("\n\r\n\rTEST START:\n\r");
+    // printSamplesCSV(x, y, FILTER_DATA_SIZE);
+    debug_out("\n\r\n\rTEST START:\n\r");
+    XTime curTime;
+    u32 uTime;
+    for (int i = 0; i < 1000; i++)
+    {
+        XTime_GetTime(&curTime);
+        uTime = curTime;
+        // debug_out("\r\n%lld - %lu - %u", curTime, uTime, uTime);
+        debug_out("\r\n%0.2f", time_diff_us(0, curTime));
+        usleep(50);
+    }
     XTime_GetTime(&startTime);
-    // for (int j = 0; j < 10; j++) {
-    // for (int i = 0; i < FILTER_DATA_SIZE; i++)
-    // {
-    //     y_fixed[i] = fir_fixed(FILTER_LENGTH, &h_fixed[0], &w_fixed[0], x_fixed[i]);
-    // }
-    // }
+    
+#define NUM_LOOPS 1
+    for (int j = 0; j < NUM_LOOPS; j++) {
+#if 1 //USE_FPGA
+    for (int i = 0; i < FILTER_DATA_SIZE; i++)
+    {
+        y_fixed[i] = fir_fixed(FILTER_LENGTH, &h_fixed[0], &w_fixed[0], x_fixed[i]);
+    }
 
-    int ret = dma_transfer((u8*)&x_fixed[0], (u8*)&y_fixed[0], sizeof(x_fixed)); // length in bytes, not array size
+#else
+    int ret = dma_transfer((u8*)x_fixed, (u8*)y_fixed, FILTER_DATA_SIZE * sizeof(fixed)); // length in bytes, not array size
+#endif
+    }
     XTime_GetTime(&endTime);
 
-    // xil_printf("\r\nReturn: %d", ret);
-    fixedToFloat(x_fixed, x, ARRAY_LEN(x));
-    fixedToFloat(y_fixed, y, ARRAY_LEN(y));
+    fixedToFloat(x_fixed, x, FILTER_DATA_SIZE);
+    fixedToFloat(y_fixed, y, FILTER_DATA_SIZE);
 
-    printSamplesCSV(&x[0], &y[0], ARRAY_LEN(y));
+    printSamplesCSV(x, y, FILTER_DATA_SIZE);
 
     
-    char str[128];
-    snprintf(str, ARRAY_LEN(str), "\n\rStart Time: %llu, End Time: %llu, Total Time: %0.1f ||| Configuration - len=%d\n\r", startTime, endTime, time_diff_us(startTime, endTime), FILTER_LENGTH);
-    xil_printf("%s", str);
+    debug_out("\n\rStart Time: %llu, End Time: %llu, Total Time: %0.1f ||| Configuration - len=%d\n\r", startTime, endTime, time_diff_us(startTime, endTime), FILTER_LENGTH);
 
     cleanup_platform();
+
+    free(x);
+    free(y);
+    free(x_fixed);
+    free(y_fixed);
     return 0;
 }
